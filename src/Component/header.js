e7788630894b758a27a915233beaa895c9ca1ffc
QianXing Ou <qxou14@gmail.com>
2023-10-29 23:06:22 -0400

    add style to headers

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { Navbar, Nav, NavItem, NavLink } from "react-bootstrap";
 
 export default function Header() {
   return (
     <div>
-      <Navbar>
-        <Navbar.Brand>
-          <a>Home</a>
-        </Navbar.Brand>
-        <Nav>
-          <NavItem eventKey={1} href="#">
+      <Navbar className="bg-body-tertiary">
+        <Navbar.Brand>Home</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link eventKey={1} href="#">
             Skills
-          </NavItem>
-          <NavItem eventKey={2} href="#">
+          </Nav.Link>
+          <Nav.Link eventKey={2} href="#">
             Experience
-          </NavItem>
-          <NavItem eventKey={3} href="#">
+          </Nav.Link>
+          <Nav.Link eventKey={3} href="#">
             Projects
-          </NavItem>
-          <NavItem eventKey={4} href="#">
+          </Nav.Link>
+          <Nav.Link eventKey={4} href="#">
             Contact Me
-          </NavItem>
+          </Nav.Link>
         </Nav>
       </Navbar>
     </div>
