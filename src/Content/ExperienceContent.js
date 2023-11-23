7da2eb205e423ea0682139397ff998edb6b358c7
QianXing Ou <qxou14@gmail.com>
2023-11-23 16:09:47 -0500

    add experience content

---
+++ src/Content/ExperienceContent.js
@@ -0,0 +1,30 @@
+const Experience = [
+  {
+    name: "Tarifica",
+    position: "Software Engineer Intern",
+    time: "June 2021 - Dec 2021",
+    description: [
+      "Designed,and rebuilt scraper software to extract data from different telecom websites.",
+      "Maintained scraper software to ensure data integrity.",
+      "Optimized and revised code to improve its robustness and readability.",
+      "Sanitized data using ETL pipeline built in Flask which empowered seamless access and usability for analysts",
+    ],
+    tools: "Python, Beautifulsoup, Selenium, Django, Flask, PostgreSQL, MySQL.",
+  },
+  {
+    name: "Citizens Financial Group, Inc",
+    position: "Software Engineer",
+    time: "June 2022 - Present",
+    description: [
+      "Build full stack enterprise microservices applications with React frontend and .NET backend.",
+      "Containerize applications using container orchestration technologies techologies such as Kubernetes and OpenShift",
+      "Automate the process of building, testing, and deploying applications by utilizing CI/CD pipeline",
+      "Write unit test with good code coverage for applications to make sure the applications run seamlessly. ",
+      "Prepare knowledge transfer sessions and documentations for the new hires on the team",
+      "Attend trainning sessions offer by the company to further improve my software engineering skills. The outcome of the training include introduction of Kafka and event driven architecture, \
+      best pratices of building restful api, risks of using opensource packages and work ethics of software engineering.",
+    ],
+    tools:
+      ".NET, react, Kubernetes, Openshift, Kafka, CICD, Restful Api, Jenkins, Datadog, Jira, Agile",
+  },
+];
