e60b7b3a2c1c5268bc370265dd637f144a88faff
QianXing Ou <qxou14@gmail.com>
2023-11-24 23:33:50 -0500

    add skill content

---
+++ src/Content/EducationContent.js
@@ -0,0 +1,19 @@
+const Education = [
+  {
+    name: "The City College of New York",
+    major: "Bachelor of Science in Computer Science",
+    gpa: "3.72",
+    year: "2018-2022",
+    relevant_course:
+      "Data Structure, Algorithms, Software Design Lab, Probability & Statistics for Computer Science, Introduction to Database Systems, Software Engineering, Programming Language Paradigms, Topics in Software Engineering, Image Processing",
+    school: true,
+  },
+  {
+    school: false,
+    name: "CodePath",
+    description: [
+      "Attended 12-week program during the spring semester which involved designing android applications that are similar to Flixster, Twitter and Instagram.",
+      "Attended 12-week program during summer to prepare, review and solve challenging algorithms problems.",
+    ],
+  },
+];
---
+++ src/Content/SkillContent.js
@@ -0,0 +1,60 @@
+// name,
+//Java, Python, C++, C#,HTML/CSS, PHP,  JavaScript ,
+//Django, React, React Native,  Firebase, Visual Studio Code, GitHub, MySQL,
+//PostgreSQL, JavaFx, IntelliJ IDEA, PyCharm, Android Studio, Code::Blocks
+const skills = [
+  {
+    name: "Python",
+    rating: 10,
+  },
+  {
+    name: "React",
+    rating: 9,
+  },
+  {
+    name: "Java",
+    rating: 8,
+  },
+  {
+    name: "C#",
+    rating: 8,
+  },
+  {
+    name: "HTML/CSS",
+    rating: 8,
+  },
+  {
+    name: "Javascript",
+    rating: 8,
+  },
+  {
+    name: "React Native",
+    rating: 8,
+  },
+  {
+    name: "Django",
+    rating: 7,
+  },
+  {
+    name: "PHP",
+    rating: 7,
+  },
+  {
+    name: "C++",
+    rating: 6,
+  },
+];
+
+const Tools = [
+  "Visual Studio",
+  "Firebase",
+  "Visual Studio Code",
+  "GitHub",
+  "MySQL",
+  "PostgreSQL",
+  "JavaFx",
+  "IntelliJ IDEA",
+  "PyCharm",
+  "Android Studio",
+  "Code::Blocks",
+];
