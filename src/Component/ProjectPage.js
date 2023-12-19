4603a7763f06008148a50f926e40428b4400091a
QianXing Ou <qxou14@gmail.com>
2023-12-19 15:38:01 -0500

    add project gif , export project file and add project content page

---
+++ src/Component/ProjectPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function ProjectPage() {
+  return <div>This is project</div>;
+}
+
+export default ProjectPage;
--- src/Content/ProjectContent.js
+++ src/Content/ProjectContent.js
@@ -134,3 +134,5 @@ const Project = [
     tool: "Java, Javafx, MySQL",
   },
 ];
+
+export default Project;
