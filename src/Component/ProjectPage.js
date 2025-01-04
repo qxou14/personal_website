b4cf0c7ddefa7fc6be95da3dd2296264dafe8b18
QianXing Ou <qxou14@gmail.com>
2025-01-03 23:06:13 -0500

    remove unused imports

--- src/Component/ProjectPage.js
+++ src/Component/ProjectPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import example from "../Assets/project1.gif";
 
 function ProjectPage({ content }) {
   return content.map((project) => (
