bd3e68d4b342e9ca6e89c1bbc24a5c83f0eb69d4
QianXing Ou <qxou14@gmail.com>
2025-02-08 23:02:19 -0500

    fixed styles

--- src/Component/ProjectPage.js
+++ src/Component/ProjectPage.js
@@ -3,42 +3,36 @@ import { Card, Carousel, Row, Col } from "react-bootstrap";
 
 function ProjectPage({ content }) {
   return (
-    <Row>
+    <div className="project-list">
       {" "}
       {content.map((project) => (
-        <Col md={6} className="single-project">
-          <Card key={project.id} className="card">
-            <Card.Body>
-              <Card.Title>{project.title}</Card.Title>
-              <Carousel
-                controls={project.gif.length > 1 ? true : false}
-                className="carousel"
-              >
-                {project.gif.map((gif, index) => {
-                  return (
-                    <Carousel.Item key={index}>
-                      <Card.Img variant="top" src={gif}></Card.Img>
-                    </Carousel.Item>
-                  );
-                })}
-              </Carousel>
-              <Card.Subtitle className="mb-2 text-muted">
-                {project.description}
-              </Card.Subtitle>
-              <div>
-                Features:
-                <ul>
-                  {project.features.map((feature, index) => (
-                    <li key={index}>{feature}</li>
-                  ))}
-                </ul>
-              </div>
-              <Card.Text>Tools: {project.tool}</Card.Text>
-            </Card.Body>
-          </Card>
+        <Col md={6} className="project" key={project.id}>
+          <div>{project.title}</div>
+          <Carousel
+            controls={project.gif.length > 1 ? true : false}
+            indicators={false}
+          >
+            {project.gif.map((gif, index) => {
+              return (
+                <Carousel.Item key={index} className="carousel-temp">
+                  <Card.Img variant="top" src={gif} className="gif-container" />
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+          <div>{project.description}</div>
+          <div>
+            Features:
+            <ul>
+              {project.features.map((feature, index) => (
+                <li key={index}>{feature}</li>
+              ))}
+            </ul>
+          </div>
+          <div>Tools: {project.tool}</div>
         </Col>
       ))}
-    </Row>
+    </div>
   );
 }
 
