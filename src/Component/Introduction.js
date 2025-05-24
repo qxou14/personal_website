0279714e39586836d9653109caf2d4044b3bca8c
QianXing Ou <qxou14@gmail.com>
2025-05-23 22:46:25 -0400

    add intro

--- src/Component/Introduction.js
+++ src/Component/Introduction.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function Introduction() {
   return (
-    <div className="introduction" id="introduction">
-      I am a passionate software engineer with 3 years of experience in
-      designing, developing, and maintaining scalable software solutions. I
-      enjoy solving complex problems and excel at writing clean and efficient
-      code.I have proven ability to work effectively and collaboratively with
-      cross-functional teams to deliver high-quality products aligned with
-      evolving enterprise goals
+    <div className="introduction-page" id="introduction">
+      <div className="name">Hello, I am QianXing Ou</div>
+      <div className="introduction-content">
+        I am a passionate software engineer with 3 years of experience in
+        designing, developing, and maintaining scalable software solutions. I
+        enjoy solving complex problems and excel at writing clean and efficient
+        code.I have proven ability to work effectively and collaboratively with
+        cross-functional teams to deliver high-quality products aligned with
+        evolving enterprise goals
+      </div>
     </div>
   );
 }
