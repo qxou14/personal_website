0d09ac6f98d06bc1cd722000e27abc4064d9f46e
QianXing Ou <qxou14@gmail.com>
2025-05-24 10:00:22 -0400

    adjust background

--- src/Component/BackgroundEffect.js
+++ src/Component/BackgroundEffect.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function BackgroundEffect() {
+  useEffect(() => {
+    const container = document.querySelector(".stars");
+    const starCount = 100;
+
+    for (let i = 0; i < starCount; i++) {
+      const star = document.createElement("div");
+      star.className = "star";
+      star.style.top = `${Math.random() * 100}%`;
+      star.style.left = `${Math.random() * 100}%`;
+      star.style.animationDelay = `${Math.random() * 5}s`;
+      container.appendChild(star);
+    }
+  }, []);
   return (
     <div className="night-sky">
+      <div className="stars"></div>
       <div className="meteor"></div>
       <div className="meteor star1"></div>
       <div className="meteor star2"></div>
